45ae6ead3785df86f58dca376457749d72c0bea7
unknown <sunshine09101@gmail.com>
2023-03-01 23:55:19 -0500

    reducer was updated

--- web/admin/react/src/constants/actionTypes.js
+++ web/admin/react/src/constants/actionTypes.js
@@ -1,7 +1,7 @@
 export const CLOSE_DRAWER = 'CLOSE_DRAWER';
 export const TOGGLE_DRAWER = 'TOGGLE_DRAWER';
 export const SET_MOBILE = 'SET_MOBILE';
-export const LOGIN = 'LOGIN';
+export const LOGIN = 'LOGIN'; 
 export const LOGOUT = 'LOGOUT';
 export const RECEIVE_PRODUCTS_LIST = 'RECEIVE_PRODUCTS_LIST';
 export const RECEIVE_PRODUCT_BEGIN = 'RECEIVE_PRODUCT_BEGIN';
